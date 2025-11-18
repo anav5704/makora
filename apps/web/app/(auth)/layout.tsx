63cb45e968837523dd405b073e910144a05cde36
Anav Chand <anavkki@gmail.com>
2025-11-18 20:14:43 +1200

    chore: create auth layout

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <main
+            className="grid w-screen grid-cols-2 p-5 h-screen items-center">
+            <section className="col-span-1 bg-zinc-800 h-full w-full rounded-xl">
+                {/* Carousel */}
+            </section>
+            <section className="col-span-1">
+                {children}
+            </section>
+        </main>
+    );
+}
