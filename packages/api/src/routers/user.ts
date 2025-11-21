378d9251dcc482f264d5a90e109ac047a6f3e7ce
Anav Chand <anavkki@gmail.com>
2025-11-21 19:29:47 +1200

    feat(api): add user onboarding

--- packages/api/src/routers/user.ts
+++ packages/api/src/routers/user.ts
@@ -1,4 +1,4 @@
-import { db } from "@makora/db";
+import { db, Platform } from "@makora/db";
 import { z } from "zod";
 import { protectedProcedure, router } from "../index";
 
@@ -6,7 +6,7 @@ export const userRouter = router({
     onboard: protectedProcedure
         .input(
             z.object({
-                platform: z.enum(["CHESS_COM", "LICHESS_ORG"]),
+                platform: z.enum([Platform.CHESS_COM, Platform.LICHESS_ORG]),
                 username: z.string(),
             }),
         )
@@ -18,7 +18,5 @@ export const userRouter = router({
                     userId: ctx.session.user.id,
                 },
             });
-
-            return {};
         }),
 });
