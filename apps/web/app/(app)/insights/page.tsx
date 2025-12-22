afcdc0f0876d4876628156be71fd741e2387707b
Anav Chand <anavkki@gmail.com>
2025-12-22 19:45:49 +1200

    feat(insights): add quick metrics

--- apps/web/app/(app)/insights/page.tsx
+++ apps/web/app/(app)/insights/page.tsx
@@ -1,3 +1,33 @@
+"use client"
+
+import { Metrics } from "@/components/insights/metrics";
+import { Loader } from "@/components/loader";
+import { api } from "@/lib/trpc";
+import { useQuery } from "@tanstack/react-query";
+
 export default function InsightsPage() {
-    return <div></div>;
+  const { data: metrics, isLoading } = useQuery(api.insights.getMetrics.queryOptions())
+
+    return (
+      <main className="h-full divide-y divide-zinc-800">
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+
+        <Metrics
+        totalLosses={metrics?.totalLosses || 0}
+        reviewedLosses={metrics?.reviewedLosses || 0}
+        averageMoves={metrics?.averageMoves || 0}
+        averageAccuracy={metrics?.averageAccuracy || 0}
+        />
+
+        <section>
+
+        </section>
+          </>
+        )}
+
+      </main>
+    );
 }
