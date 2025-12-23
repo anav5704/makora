cbfdd61479211dfc1dfc2d274f5043ee7fb674f6
Anav Chand <anavkki@gmail.com>
2025-12-23 21:25:26 +1200

    feat(insights): add over time charts

--- apps/web/app/(app)/insights/page.tsx
+++ apps/web/app/(app)/insights/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 
+import { AreaChart } from "@/components/charts/areaChart";
 import { Metrics } from "@/components/insights/metrics";
 import { Loader } from "@/components/loader";
 import { api } from "@/lib/trpc";
 import { useQuery } from "@tanstack/react-query";
 
 export default function InsightsPage() {
-  const { data: metrics, isLoading } = useQuery(api.insights.getMetrics.queryOptions())
+  const { data: metrics, isLoading: metricsLoading } = useQuery(api.insights.getMetrics.queryOptions())
+  const { data: overTimeData, isLoading: overTimeLoading } = useQuery(api.insights.getOverTime.queryOptions())
+
+  const isLoading = metricsLoading || overTimeLoading
 
     return (
       <main className="h-full divide-y divide-zinc-800">
@@ -22,9 +26,23 @@ export default function InsightsPage() {
         averageAccuracy={metrics?.averageAccuracy || 0}
         />
 
-        <section>
+          <AreaChart
+            title="Games Lost"
+            data={overTimeData || []}
+            xKey="date"
+            yKey="losses"
+            unit=" Losses"
+          />
 
-        </section>
+          <AreaChart
+            title="Average Accuracy"
+            data={overTimeData || []}
+            xKey="date"
+            yKey="accuracy"
+            unit="%"
+          />
+
+          <div />
           </>
         )}
 
