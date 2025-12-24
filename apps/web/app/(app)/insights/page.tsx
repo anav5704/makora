32c1450351c2da686f86b3f7e8164feb6a9ce106
Anav Chand <anavkki@gmail.com>
2025-12-24 21:10:32 +1200

    feat(insights): add stat distribution chart

--- apps/web/app/(app)/insights/page.tsx
+++ apps/web/app/(app)/insights/page.tsx
@@ -2,6 +2,7 @@
 
 import { AreaChart } from "@/components/charts/areaChart";
 import { BarList } from "@/components/charts/barList";
+import { DonutChart } from "@/components/charts/donutChat";
 import { Metrics } from "@/components/insights/metrics";
 import { Loader } from "@/components/loader";
 import { api } from "@/lib/trpc";
@@ -11,11 +12,12 @@ export default function InsightsPage() {
   const { data: metrics, isLoading: metricsLoading } = useQuery(api.insights.getMetrics.queryOptions())
   const { data: overTimeData, isLoading: overTimeLoading } = useQuery(api.insights.getOverTime.queryOptions())
   const { data: comparisonData, isLoading: comparisonLoading } = useQuery(api.insights.getComparison.queryOptions())
+  const { data: distributionData, isLoading: distributionLoading } = useQuery(api.insights.getDistribution.queryOptions())
 
-  const isLoading = metricsLoading || overTimeLoading || comparisonLoading
+  const isLoading = metricsLoading || overTimeLoading || comparisonLoading || distributionLoading
 
     return (
-      <main className="h-full divide-y divide-zinc-800">
+      <main className="h-full divide-y divide-x divide-zinc-800">
         {isLoading ? (
           <Loader />
         ) : (
@@ -28,7 +30,7 @@ export default function InsightsPage() {
         averageAccuracy={metrics?.averageAccuracy || 0}
         />
 
-          <AreaChart
+        <AreaChart
             title="Games Lost"
             data={overTimeData ?? []}
             xKey="date"
@@ -48,6 +50,35 @@ export default function InsightsPage() {
             title="Top Openings"
             data={comparisonData ?? []}
           />
+
+          <section className="grid grid-cols-3 divide-x divide-y divide-zinc-800">
+
+
+          <DonutChart
+            title="Time Control"
+            data={distributionData?.timeControl ?? []}
+          />
+          <DonutChart
+            title="Game Phase"
+            data={distributionData?.gamePhase ?? []}
+          />
+          <DonutChart
+            title="Termination"
+            data={distributionData?.termination ?? []}
+          />
+          <DonutChart
+            title="Day of Week"
+            data={distributionData?.dayOfWeek ?? []}
+          />
+          <DonutChart
+            title="Color"
+            data={distributionData?.color ?? []}
+          />
+          <DonutChart
+            title="Platform"
+            data={distributionData?.platform ?? []}
+          />
+                 </section>
           </>
         )}
 
