dbbb97d7f865151fece6dd32cdd95486095fa179
Anav Chand <anavkki@gmail.com>
2026-01-03 21:35:48 +1200

    feat(ui): add search component

---
+++ apps/web/components/games/search.tsx
@@ -0,0 +1,36 @@
+"use clien";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+
+export const Search = () => {
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    const [query, setQuery] = useState(searchParams.get("search"));
+
+    const handleSearch = () => {
+        const params = new URLSearchParams(searchParams);
+
+        if (query) {
+            params.set("search", query);
+        } else {
+            params.delete("search");
+        }
+
+        router.replace(`${pathname}?${params.toString()}` as any);
+    };
+
+    return (
+        <Input
+            name="search"
+            type="text"
+            value={query || ""}
+            placeholder="Search by opening or opponent"
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+        />
+    );
+};
