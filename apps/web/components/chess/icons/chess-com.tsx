da6417c346d663b48ae4c6af346e0e5ca87e4339
Anav Chand <anavkki@gmail.com>
2026-01-05 21:54:59 +1200

    chore(ui): change chess.com and lichess.org logo size

--- apps/web/components/chess/icons/chess-com.tsx
+++ apps/web/components/chess/icons/chess-com.tsx
@@ -1,8 +1,8 @@
 export const ChessCom = () => {
     return (
         <svg
-            height={24}
-            width={24}
+            height={22}
+            width={22}
             role="img"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
