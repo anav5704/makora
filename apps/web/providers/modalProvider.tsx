4c61845578a4ab22b0aab3d7e16e92e1fd304732
Anav Chand <anavkki@gmail.com>
2026-01-30 22:24:21 +1200

    feat(settings): add edit account

--- apps/web/providers/modalProvider.tsx
+++ apps/web/providers/modalProvider.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { AddAccount } from "@/components/modals/addAccount";
+import { EditAccount } from "@/components/modals/editAccount";
 import { FilterGame } from "@/components/modals/filterGame";
 
 export const ModalProvider = () => {
   return (
       <>
         <AddAccount />
+        <EditAccount />
         <FilterGame />
       </>
     )
